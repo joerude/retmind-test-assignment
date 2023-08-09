2421b4b75891dab4e27d4825ca9d00aca23c2a21
Zhoomart Cholponbek uulu <ma4akk@gmail.com>
2023-08-09 15:25:38 +0600

    Added endpoint for downloading the  product list as an Excel file

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ProductListView
+from .views import ProductListView, ProductExportView
 
 urlpatterns = [
     path('products/', ProductListView.as_view(), name='product-list'),
+    path('products/export/', ProductExportView.as_view(), name='product-export-excel'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,8 +1,12 @@
+import openpyxl
+
 from rest_framework import generics, status
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
 
 from django.core.cache import cache
+from django.http import HttpResponse
 
 from .models import Product
 from .serializers import ProductSerializer
@@ -30,3 +34,31 @@ class ProductListView(generics.ListAPIView):
         queryset = self.get_queryset()
         serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+class ProductExportView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+
+        workbook = openpyxl.Workbook()
+        sheet = workbook.active
+        sheet.title = "Product List"
+        sheet.append(["ID", "Name", "Price", "Created At"])
+
+        for product_data in serializer.data:
+            sheet.append(
+                [
+                    product_data['id'], product_data['name'],
+                    product_data['price'], product_data['created_at']
+                ]
+            )
+
+        response = HttpResponse(content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+        response['Content-Disposition'] = 'attachment; filename=product_list.xlsx'
+        workbook.save(response)
+        workbook.close()
+
+        return response
