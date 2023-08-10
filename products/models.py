2466510441447dd56fdc4ab0d151469da1c337a1
Zhoomart Cholponbek uulu <ma4akk@gmail.com>
2023-08-10 15:35:20 +0600

    added validation for price field

--- products/models.py
+++ products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
 
 class Category(models.Model):
@@ -28,7 +29,7 @@ class Product(models.Model):
     tags = models.ManyToManyField(Tag, related_name='tagged_products')
     name = models.CharField(max_length=100)
     description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    price = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(0)])
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
